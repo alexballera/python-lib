4e5baa4a11533ef79629b5a2c67d1c8dddf90934
alexballera <alexballera@gmail.com>
2025-12-04 07:12:21 -0300

    Update comments for clarity and consistency in list operations

--- python/baraa/list.py
+++ python/baraa/list.py
@@ -12,7 +12,7 @@ print(lista[:3])
 print(lista[7:])
 print(lista[1:5])
 #%%
-# Deben coincidir las variables o usar el *resto
+# Deben coincidir las variables, si no se declaran todas las variables entonces usar el *resto
 uno, dos, tres, *resto = lista
 print(uno)
 print(dos)
@@ -22,12 +22,12 @@ primero, *resto, ultimo = lista
 print(primero)
 print(resto)
 print(ultimo)
-#%%
+#%% underscore obvia la declaración de esa variable
 primero, _, _, cuatro, _ , *resto = lista
 print(primero)
 print(cuatro)
 print(resto)
-#%%
+#%% *_ obvia el resto de las variables
 primero, *_ = lista
 print(primero)
 #%%
@@ -38,21 +38,22 @@ min(lista)
 sum(lista)
 #%%
 len(lista)
-#%%
-all(lista) # todos verdaderos
+#%% % devuelve True si se cumplen TODOS
+all(lista)
 #%%
 all([1,0,6])
-#%%
+#%
 all(['s', '', 'y'])
-#%%
+#%% any devuelve True si cumple AL MENOS UNO
 any(lista)
 #%%
 any(['s', '', 'y'])
 #%%
 lista.count(5)
-#%%
+#%% cuenta la cantidad de veces que se encuentra el elemento que se pasa por parámetro
 lista.append(5)
 lista.count(5)
 # %%
 print(9 in lista)
 print(9 not in lista)
+# %%
