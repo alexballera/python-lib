d558d0f096450908143899e1d47b05b193e60f34
alexballera <alexballera@gmail.com>
2025-12-05 16:30:53 -0300

    Refactor pandas_basico.py to improve readability and add DataFrame examples

--- python/a2-capacitacion/pandas/pandas_basico.py
+++ python/a2-capacitacion/pandas/pandas_basico.py
@@ -6,19 +6,20 @@
 #%%
 import numpy as np
 import pandas as pd
-#%% Series
+#%% ======================================================================
+# Series
 altura_cm = [178, 165, 155, 181, 176, 189, 190, 186]
 
 altura = pd.Series(altura_cm)
 altura.name = 'Alturas'
 altura # type: ignore
 #%% observo algunos elementos
-altura.head() # primeros 5 elementos
-altura.head(3) # la cantidad que quiero
-altura.tail() # últimos 5
-altura.tail(3) # los últimos que quiero
-altura.sample() # muestra uno aleatorio
-altura.sample(3) # n aleatorios
+print(f'\nAlturas primeros 5: \n{altura.head()}') # primeros 5 elementos
+print(f'\nAlturas primeros 3: \n{altura.head(3)}') # la cantidad que quiero
+print(f'\nAlturas últimos 5: \n{altura.tail()}') # últimos 5
+print(f'\nAlturas últimos 3: \n{altura.tail(3)}') # los últimos que quiero
+print(f'\nAlturas uno aleatorio: \n{altura.sample()}') # muestra uno aleatorio
+print(f'\nAlturas 3 aleatorios: \n{altura.sample(3)}') # n aleatorios
 #%% ordenar
 altura.sort_values() # ordena ascendente
 altura.sort_values(ascending=False) # ordena descendente
@@ -43,8 +44,10 @@ altura2 = pd.Series(altura2)
 altura2.name = 'Altura2'
 
 print(f'Válidos: {altura2.count()}')
-print(f'¿Todos Válidos?: {altura2.isna().all()}\n')
+print(f'¿altura tiene algún valor NaN?: {altura.hasnans}\n')
+print(f'¿altura2 tiene algún valor NaN?: {altura2.hasnans}\n')
 altura2.info()
+altura2[8]
 
 #%% Filtros
 alto = 170
@@ -54,4 +57,27 @@ mas_altos = pd.Series(mas_altos) # Hay que hacer un paso más para convertir la 
 condicion = altura.gt(alto) # gt = greater than alto (altura > alto) 
 mas_altos2 = altura[condicion] # Estilo pandas (recomendable)
 mas_altos2
+#%% ======================================================================
+# DataFrames
+datos = {
+    "nombre": ['Erika', 'Elías', 'Miguel', 'Karla', 'Gabriela'],
+    "edad": [23, 24, 21, 25, 22],
+    "altura": [173, 194, 165, 154, 176],
+    "peso": [83.2, 65.4, 71.1, 67.9, 71.8]
+}
+#%%
+amigos = pd.DataFrame(datos)
+amigos
+#%%
+amigos.describe()
+#%%
+amigos.info()
+#%% Filtros 
+# edad < 23
+edad = 23
+condicion = amigos['edad'] < edad
+filtrado = amigos[condicion]
+print(f'Filtrado: \n {filtrado}')
+print(f'\nDescribe: \n {filtrado.describe()}')
+print(f'\n Edad máxima: {filtrado['edad'].max()}')
 #%%
