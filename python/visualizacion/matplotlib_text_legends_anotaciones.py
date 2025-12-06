e731cf831bfb10c42cf0745ace01411aa64eea5e
alexballera <alexballera@gmail.com>
2025-12-06 14:05:47 -0300

    Enhance Matplotlib examples with text annotations and customized arrow properties

--- python/visualizacion/matplotlib_text_legends_anotaciones.py
+++ python/visualizacion/matplotlib_text_legends_anotaciones.py
@@ -21,8 +21,10 @@ Y = [3, 4, 5, 3, 2, 4, 5, 7]
 
 fig, ax = plt.subplots(figsize=figsize, layout=layout)
 
+ax.plot(X, Y)
+
 fontdict = dict(
-            c = 'yellow', # color
+            c = 'purple', # color
             family = 'monospace', # {FONTNAME, 'serif', 'sans-serif', 'cursive', 'fantasy', 'monospace'}
             size = 'xx-large', # float or {'xx-small', 'x-small', 'small', 'medium', 'large', 'x-large', 'xx-large'}
             style = 'italic', # 	{'normal', 'italic', 'oblique'}
@@ -32,17 +34,17 @@ fontdict = dict(
             )
 
 bbox=dict(
-        fc='blue', # facecolor o backgroundColor
-        ec='red', # edgecolor o borde
+        fc='coral', # facecolor o backgroundColor
+        ec='blue', # edgecolor o borde
         alpha=0.8,
-        joinstyle='round',
         ls='--', # linestyle {'-', '--', '-.', ':', '', (offset, on-off-seq), ...}
         lw= 3.0, # linewidth
         fill=True, # por default True, pero False sin relleno
-        boxstyle='round',
+        boxstyle='sawtooth', # square, circle, ellipse, larrow, rarrow, darrow, round, round4, sawtooth, roundtooth
+        pad=0.7, # padding
         )
 
-ax.plot(X, Y)
+
 ax.text(x=5,
         y=5,
         s='Hola Mundo',
@@ -57,10 +59,25 @@ plt.show()
 fig, ax = plt.subplots(figsize=figsize, layout=layout)
 
 ax.plot(X, Y)
+
+bbox=dict(
+        fc='coral', # facecolor o backgroundColor
+        ec='blue', # edgecolor o borde
+        alpha=0.8,
+        ls='--', # linestyle {'-', '--', '-.', ':', '', (offset, on-off-seq), ...}
+        lw= 3.0, # linewidth
+        fill=True, # por default True, pero False sin relleno
+        boxstyle='larrow', # square, circle, ellipse, larrow, rarrow, darrow, round, round4, sawtooth, roundtooth
+        pad=0.7, # padding
+        )
+
+alpha = 20
+delta = 32
+
 ax.text(x=5,
         y=5,
-        s='Hola Mundo', # string o texto
-        c = 'yellow', # color
+        s=fr'Hola Mundo $\alpha = {alpha}, delta = {delta}$', # string o texto
+        c = 'purple', # color
         family = 'monospace', # {FONTNAME, 'serif', 'sans-serif', 'cursive', 'fantasy', 'monospace'}
         size = 'xx-large', # float or {'xx-small', 'x-small', 'small', 'medium', 'large', 'x-large', 'xx-large'}
         style = 'italic', # 	{'normal', 'italic', 'oblique'}
@@ -69,5 +86,57 @@ ax.text(x=5,
         weight = 1000, # {a numeric value in range 0-1000, 'ultralight', 'light', 'normal', 'regular', 'book', 'medium', 'roman', 'semibold', 'demibold', 'demi', 'bold', 'heavy', 'extra bold', 'black'}
         bbox=bbox
         )
+
+plt.show()
+
+#%% ANOTACIONES
+# https://matplotlib.org/stable/api/_as_gen/matplotlib.axes.Axes.annotate.html#matplotlib.axes.Axes.annotate
+
+fig, ax = plt.subplots(figsize=figsize, layout=layout)
+
+ax.plot(X, Y)
+
+arrowprops=dict(
+        width=8,
+        headwidth=20,
+        headlength=20,
+        shrink=0.05,
+        facecolor='r',
+        edgecolor='blue',
+        lw=2,
+    )
+
+ax.annotate(
+    text=fr'Hola Mundo $\alpha = {alpha}$',
+    xy=(3,5),
+    xytext=(6,6),
+    fontsize=16,
+    arrowprops=arrowprops,
+)
+
+plt.show()
+
+#%%
+fig, ax = plt.subplots(figsize=figsize, layout=layout)
+
+ax.plot(X, Y)
+
+arrowprops=dict(
+        arrowstyle="fancy",
+        facecolor='r',
+        edgecolor='blue',
+        lw=2,
+    )
+
+bbox['boxstyle'] = 'square'
+
+ax.annotate(
+    text=fr'Hola Mundo $\alpha = {alpha}$',
+    xy=(6,4),
+    xytext=(6,3),
+    fontsize=16,
+    arrowprops=arrowprops,
+    bbox=bbox
+)
 
 plt.show()
